7e44f877aeaeb7d186427505fcd685a54ae93323
jjanku <joan.janku@ikubinfo.al>
2022-08-31 11:32:49 +0200

    Basic Kotlin

---
+++ src/main/kotlin/Alien.kt
@@ -0,0 +1,11 @@
+class Alien(private val name: String, private val age: Int) {
+
+
+    fun name(): String {
+        return this.name
+    }
+
+    fun age(): Int {
+        return this.age
+    }
+}
---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("hello wol")
+
+    val alien = Alien("elene",22)
+
+    println("Name is :  ${alien.name()} ")
+    println("Age is :  ${alien.age()} ")
+
+    val num1 = 5;
+    val num2 = 6;
+    println("Result is ${num1+num2}")
+}
